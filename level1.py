0f5dd1a0f7fe9a0bf1acad9c749d92f60d9c97fc
Radmaster5000 <phil.radford5000@gmail.com>
2021-09-13 23:32:53 +0100

    made it to level 2

--- level1.py
+++ level1.py
@@ -1,3 +1,15 @@
 string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-print(chr(ord('k')+2))
+url = "http://www.pythonchallenge.com/pc/def/map.html"
+
+#for i in string:
+#	if i.isalpha():
+#		print(chr(ord(i)+2), end='')
+#	else:
+#		print(i, end='')
+
+intab = "abcdefghijklmnopqrstuvwxyz"
+outtab = "cdefghijklmnopqrstuvwxyzab"
+trantab = url.maketrans(intab, outtab)
+
+print(url.translate(trantab))
